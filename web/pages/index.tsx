c5c872ac43e2e1f1a81e7f912b2e927cee8cfd18
ndzhwr <ndizihiweregis06@gmail.com>
2023-01-20 22:08:19 +0200

    Initial commit

---
+++ web/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { type NextPage  } from "next";
+
+
+const Home : NextPage = () => {
+  
+return <h1 className="text-xl text-center mt-10 ">The quick brown fox</h1>
+
+}
+
+export default Home

900008babbece38529d72d3cc5c367e6b0d39089
ndzhwr <ndizihiweregis06@gmail.com>
2023-01-22 22:04:38 +0200

    feat: cloudinary file upload helper fn

--- api/src/utils/utils.service.ts
+++ api/src/utils/utils.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AuthTokens } from '../types';
 import { JwtService } from '@nestjs/jwt';
-interface JwtPayload {
-  id: string;
-  username: string;
-}
+import { JwtPayload } from '../types';
+import { v2 } from 'cloudinary';
+import { env } from 'process';
+import { config } from 'dotenv';
+config();
 
 @Injectable()
 export class UtilsService {
@@ -24,5 +25,23 @@ export class UtilsService {
         expiresIn: '7d',
       }),
     };
+  }
+
+  async uploadFile(file: Express.Multer.File): Promise<string> {
+    try {
+      const base64 = file.buffer.toString('base64');
+      v2.config({
+        cloud_name: env.CLOUDINARY_CLOUD_NAME,
+        api_key: env.CLOUDINARY_API_KEY,
+        api_secret: env.CLOUDINARY_API_SECRET,
+      });
+      const result = await v2.uploader.upload(
+        'data:image/png;base64,' + base64,
+      );
+      return result.secure_url;
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(err);
+    }
   }
 }

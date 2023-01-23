5c0c16540a5fd63513c586f7422405480e7c1f65
ndzhwr <ndizihiweregis06@gmail.com>
2023-01-23 18:25:33 +0200

    feat: parse gender pipe

---
+++ api/src/pipes/parsegenderpipe/parsegender.pipe.ts
@@ -0,0 +1,34 @@
+import {
+  Injectable,
+  NotAcceptableException,
+  PipeTransform,
+} from '@nestjs/common';
+import { GENDER } from '@prisma/client';
+import { GENDER as STRING_GENDER } from 'src/types';
+
+@Injectable()
+export class ParseGenderPipe implements PipeTransform {
+  transform(value: any) {
+    const received_gender: STRING_GENDER = value.gender;
+    if (!received_gender) {
+      return value;
+    } else {
+      let gender: GENDER = GENDER.MALE;
+      switch (received_gender) {
+        case 'FEMALE':
+          gender = GENDER.FEMALE;
+          break;
+        case 'MALE':
+          gender = GENDER.MALE;
+          break;
+        case 'PREFER_NOT_TO_SAY':
+          gender = GENDER.PREFER_NOT_TO_SAY;
+          break;
+        default:
+          throw new NotAcceptableException('Invalid gender value');
+      }
+      value.gender = gender;
+      return value;
+    }
+  }
+}

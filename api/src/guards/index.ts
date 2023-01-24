9ae71664690197c2cb341f44bed82a59af04ad7a
ndzhwr <ndizihiweregis06@gmail.com>
2023-01-24 21:42:57 +0200

    feat(api): post owner guard

---
+++ api/src/guards/index.ts
@@ -0,0 +1,1 @@
+export * from './postowner.guard';
---
+++ api/src/guards/postowner.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable, CanActivate, Req, Body, ExecutionContext, NotFoundException } from "@nestjs/common";
+import { Request } from "express";
+import { PrismaClient } from "@prisma/client";
+@Injectable()
+export class PostOwnerGuard implements CanActivate {
+    private prisma : PrismaClient;
+    constructor(){
+        this.prisma = new PrismaClient();
+    }
+    async canActivate(
+        context: ExecutionContext
+    ): Promise<boolean> {
+        const req: Request = context.switchToHttp().getRequest();
+        const userId =  req.user['id']  
+        const postId = req.body.postId
+
+        const post = await this.prisma.post.findUnique({
+            where: {
+                id: postId
+            }
+        })
+        if(!post) throw new NotFoundException("Post not found")
+        return post.authorId === userId
+    }
+}

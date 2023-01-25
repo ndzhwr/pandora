5b8308e549820f9242366444a7c9bf2a155cba44
ndzhwr <ndizihiweregis06@gmail.com>
2023-01-25 07:08:55 +0200

    fix(api): move data from body to query

--- api/src/guards/postowner.guard.ts
+++ api/src/guards/postowner.guard.ts
@@ -1,25 +1,30 @@
-import { Injectable, CanActivate, Req, Body, ExecutionContext, NotFoundException } from "@nestjs/common";
-import { Request } from "express";
-import { PrismaClient } from "@prisma/client";
+import {
+  Injectable,
+  CanActivate,
+  Req,
+  Body,
+  ExecutionContext,
+  NotFoundException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PostOwnerGuard implements CanActivate {
-    private prisma : PrismaClient;
-    constructor(){
-        this.prisma = new PrismaClient();
-    }
-    async canActivate(
-        context: ExecutionContext
-    ): Promise<boolean> {
-        const req: Request = context.switchToHttp().getRequest();
-        const userId =  req.user['id']  
-        const postId = req.body.postId
+  private prisma: PrismaClient;
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req: Request = context.switchToHttp().getRequest();
+    const userId = req.user['id'];
+    const postId = req.query['postId']
 
-        const post = await this.prisma.post.findUnique({
-            where: {
-                id: postId
-            }
-        })
-        if(!post) throw new NotFoundException("Post not found")
-        return post.authorId === userId
-    }
+    const post = await this.prisma.post.findUnique({
+      where: {
+        id: postId as string,
+      },
+    });
+    if (!post) throw new NotFoundException('Post not found');
+    return post.authorId === userId;
+  }
 }

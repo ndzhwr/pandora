6a0285df669e3b3e8115772821cf88d943f5745b
ndzhwr <ndizihiweregis06@gmail.com>
2023-02-15 19:51:37 +0200

    fix: cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,14 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { config } from 'dotenv';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 import { env } from 'process';
 declare const module: any;
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(env.PORT);
-  app.use(cors());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Access-Control-Allow-Origin'],
+  });
+  // app.use(
+  //   cors({
+  //     origin: [
+  //       'http://localhost:3000',
+  //       'https://pandora-monorepo-web.vercel.com',
+  //     ],
+  //     credentials: true,
+  //     optionsSuccessStatus: 200,
+  //   }),
+  // );
   app.useLogger(new Logger());
   if (module.hot) {
     module.hot.accept();

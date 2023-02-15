e64419dec7d743df8af8e683e15d0e12c11e2312
ndzhwr <ndizihiweregis06@gmail.com>
2023-02-15 17:13:39 +0200

    fix(api): fixed cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { config } from 'dotenv';
+import * as cors from 'cors';
 import { env } from 'process';
 declare const module: any;
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(env.PORT);
+  app.use(cors());
   app.useLogger(new Logger());
   if (module.hot) {
     module.hot.accept();

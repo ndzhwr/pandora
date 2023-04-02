ecf59c83c3ce71a910aa08fac18eb1aab6088833
ndzhwr <ndizihiweregis06@gmail.com>
2023-04-02 11:13:55 -0700

    fix(api): error handling

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
@@ -14,6 +14,7 @@ export class PrismaService extends PrismaClient {
   }
   onModuleInit() {
     this.$connect();
+    Logger.log("Database connected successfully", "DatabaseConnector")
   }
   onModuleDestroy() {
     this.$disconnect();
--- web/utils/api.ts
+++ web/utils/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { deleteCookies, getCookie, setCookie } from './cookie'
+import { log } from 'console';
 export const api = () => {
     const token = getCookie("accessToken");
     return axios.create({
@@ -26,36 +27,42 @@ interface FetcherOptions {
 export const fetcher = async (url: string, options: FetcherOptions) => {
     let response: any;
     console.log(options);
-    let res = await fetch(`http://localhost:5000/${url}`, {
-        method: options.method || "GET",
-        body: JSON.stringify(options.body),
-        headers: {
-            "Content-Type": options.c_type ? options.c_type : "application/json",
-            "authorization": options.useToken ? `Bearer ${getCookie("accessToken")}` : null,
-            ...options.headers
-        }
-    })
-    response = await res.json()
-    console.log(response);
+    try {
 
-    if (response.message != undefined && response.message.name == "TokenExpiredError") {
-        deleteCookies();
-        let resp = await fetch("http://localhost:5000/auth/refreshToken", {
-            method: "PUT",
-            body: JSON.stringify({ refreshToken: getCookie("refreshToken") }),
+        let res = await fetch(`http://localhost:5000/${url}`, {
+            method: options.method || "GET",
+            body: JSON.stringify(options.body),
             headers: {
-                "Content-type": "application/json"
+                "Content-Type": options.c_type ? options.c_type : "application/json",
+                "authorization": options.useToken ? `Bearer ${getCookie("accessToken")}` : null,
+                ...options.headers
             }
         })
-        resp = await resp.json()
-        console.log("Response", resp);
-        if (resp.status == 200) {
-            setCookie("accessToken", resp["tokens"].accessToken)
-            setCookie("refreshToken", resp["tokens"].refreshToken)
-            fetcher(url, options)
+        response = await res.json()
+        console.log(response);
+
+        if (response.message != undefined && response.message.name == "TokenExpiredError") {
+            deleteCookies();
+            let resp = await fetch("http://localhost:5000/auth/refreshToken", {
+                method: "PUT",
+                body: JSON.stringify({ refreshToken: getCookie("refreshToken") }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+            resp = await resp.json()
+            console.log("Response", resp);
+            if (resp.status == 200) {
+                setCookie("accessToken", resp["tokens"].accessToken)
+                setCookie("refreshToken", resp["tokens"].refreshToken)
+                fetcher(url, options)
+            }
+        } else {
+            return response
         }
-    } else {
-        return response
+    } catch (error) {
+        console.log(error);
+        return "Something went wrong"
     }
 
 }
